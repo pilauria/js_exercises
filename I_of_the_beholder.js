49385410cee6e0d53337406741737c8474f81f14
Pietro Lauria <ptr.lauria@gmail.com>
2021-05-07 13:32:42 +0200

    Final refactoring

--- I_of_the_beholder.js
+++ I_of_the_beholder.js
@@ -1,5 +1,6 @@
 /* Write a program using nodejs that accepts a number and then prints the letter I */
 
+// No refactoring
 // let displayI = function (number) {
 //   let star = '*';
 //   for (let i = 1; i <= number; i++) {
@@ -15,16 +16,33 @@
 //   }
 // };
 
-let displayI = function (number) {
-  let star = '*';
+// My refactoring
+// let displayI = function (number) {
+//   let star = '*';
 
-  for (let i = 1; i <= number; i++) {
-    if (i === 1 || i === number) console.log(star.repeat(number));
-    if (i > 1 && i < number) {
-      number % 2 === 1
-        ? console.log(star.padStart((number - 1) / 2 + 1))
-        : console.log((star + star).padStart(number / 2 + 1));
-    }
+//   for (let i = 1; i <= number; i++) {
+//     if (i === 1 || i === number) console.log(star.repeat(number));
+//     if (i > 1 && i < number) {
+//       number % 2 === 1
+//         ? console.log(star.padStart((number - 1) / 2 + 1))
+//         : console.log((star + star).padStart(number / 2 + 1));
+//     }
+//   }
+// };
+// console.log(displayI(3));
+
+// Final refactoring
+const displayI = number => {
+  let star = '*';
+  console.log(star.repeat(number));
+  for (let i = 1; i <= number - 2; i++) {
+    const isOdd = number % 2 === 1;
+    const stars = star.repeat(isOdd ? 1 : 2);
+    const spaces = ' '.repeat(
+      isOdd ? Math.floor(number / 2) : Math.floor(number / 2) - 1
+    );
+    console.log(`${spaces}${stars}`);
   }
+  console.log(star.repeat(number));
 };
-console.log(displayI(3));
+displayI(9);
