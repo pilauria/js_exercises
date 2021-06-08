8148da56fa047cca67ac348669626286887c7ee6
Pietro Lauria <ptr.lauria@gmail.com>
2021-06-08 19:09:41 +0200

    .

--- test.js
+++ test.js
@@ -1,49 +1,46 @@
-// For all of these, what is the value of a when the function gets called with the alert()
-// #1
-// function q1() {
-//   var a = 5;
-//   if (a > 1) {
-//     a = 3;
+//#1 change this function into a ternary and assign it to variable called experiencePoints
+// function experiencePoints() {
+//   if (winBattle()) {
+//     return 10;
+//   } else {
+//     return 1;
 //   }
-//   console.log(a);
 // }
 
-// q1();
+// const experiencePoints = winbattle() ? 10 : 1;
 
-// //#2
-// var a = 0;
-// function q2() {
-//   a = 5;
-// }
-
-// function q22() {
-//   console.log(a);
-// }
-
-// q2();
-// q22();
-// console.log(a);
-
-// //#3
-// function q3() {
-//   window.a = 'hello';
-// }
-
-// function q32() {
-//   console.log(a);
-// }
-
-// //#4
-// var a = 1;
-// function q4() {
-//   var a = 'test';
-//   console.log(a);
-// }
-
-//#5
-let a = 2;
-if (true) {
-  let a = 5;
-  console.log(a);
+//Using this function, answer the questions below:
+function moveCommand(direction) {
+  var whatHappens;
+  switch (direction) {
+    case 'forward':
+      break;
+      whatHappens = 'you encounter a monster';
+    case 'back':
+      whatHappens = 'you arrived home';
+      break;
+      break;
+    case 'right':
+      return (whatHappens = 'you found a river');
+      break;
+    case 'left':
+      break;
+      whatHappens = 'you run into a troll';
+      break;
+    default:
+      whatHappens = 'please enter a valid direction';
+  }
+  return whatHappens;
 }
-console.log(a);
+
+console.log(moveCommand(forward));
+
+//#2 return value when moveCommand("forward");
+
+//#3 return value when moveCommand("back");
+
+//#4 return value when moveCommand("right");
+
+//#5 return value when moveCommand("left");
+
+//BONUS: practice makes perfect. Go and write your own switch function. It takes time to get used to the syntax!
