2709b9954a42872868dd5f18795e43603195064a
Pietro Lauria <ptr.lauria@gmail.com>
2021-06-10 16:05:20 +0200

    Map-Filter

--- arrayExercises.js
+++ arrayExercises.js
@@ -26,33 +26,61 @@ const array = [
   },
 ];
 
-const zed = [1, 2, 3];
+// const zed = [1, 2, 3];
 
-const newZed = [];
-zed.forEach(num => newZed.push(`${num}!`));
-console.log(newZed);
+// const newZed = [];
+// zed.forEach(num => newZed.push(`${num}!`));
+// console.log(newZed);
 
 // Create an array using forEach that has all the usernames with a "!" to each of the usernames
-console.log(array[0].username);
 
 const newArr = [];
-array.forEach((_, idx) => {
-  newArr.push(`${array[idx].username}!`);
+array.forEach(user => {
+  newArr.push(`${user.username}!`);
 });
 console.log(newArr);
 
+// OR:
+// const newArr = [];
+// array.forEach((_, idx) => {
+//   newArr.push(`${array[idx].username}!`);
+// });
+// console.log(newArr);
+
+//OR:
+// let newArray = [];
+// array.forEach(user => {
+//   let { username } = user;
+//   username = username + '!';
+//   newArray.push(username);
+// });
+// console.log(newArray);
+
 // Create an array using map that has all the usernames with a "? to each of the usernames
-const mapArray = array.map((_, idx) => {
-  return `${array[idx].username}?`;
+const mapArray = array.map(user => {
+  return `${user.username}?`;
 });
 
 console.log(mapArray);
 
+// OR:
+// const mapArray = array.map((_, idx) => {
+//   return `${array[idx].username}?`;
+// });
+// console.log(mapArray);
+
+// OR:
+// const mapArray = array.map(user => {
+//   let { username } = user;
+//   return username + '?';
+// });
+// console.log(mapArray);
+
 // Filter the array to only include users who are on team: red
 // (Filtering an array of objects)
 
-const filterArray = array.filter(array => {
-  return array.team === 'red';
+const filterArray = array.filter(user => {
+  return user.team === 'red';
 });
 
 console.log(filterArray);
@@ -65,11 +93,22 @@ const sumArrayUsers = array.reduce((total, currentValue) => {
 
 console.log(sumArrayUsers);
 
-// (1), what is the value of i?
+// (1), what is the value of i? INDEX of the array.
 // (2), Make this map function pure:
 const arrayNum = [1, 2, 4, 5, 8, 9];
 const newArray = arrayNum.map(num => {
   return num * 2;
 });
 
-// //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+// //BONUS: create a new list with all user information, but add "!" to the end of each items they own:
+// ===> Map an array of arrays (https://stackoverflow.com/questions/35325767/map-an-array-of-arrays)
+const newArray = array.map(user => {
+  //first map: iterate over all the items and add '!' to each of them
+  user.items = user.items.map(item => {
+    // console.log(item);
+    return `${item}!`;
+  });
+  //second map: iterate over the objects and simply return them to recompose the array
+  return user;
+});
+console.log(newArray);
