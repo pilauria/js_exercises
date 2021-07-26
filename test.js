6131d02d67ad0483c0c3acb4d93fbcab6d5921ba
Pietro Lauria <ptr.lauria@gmail.com>
2021-07-26 16:09:57 +0200

    test

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-function largestSwap(number) {
-  digitOne = number.toString().charAt(0);
-  digitTwo = number.toString().charAt(1);
-  const value = digitTwo > digitOne ? false : true;
-  return value;
-}
+// function largestSwap(number) {
+//   digitOne = number.toString().charAt(0);
+//   digitTwo = number.toString().charAt(1);
+//   const value = digitTwo > digitOne ? false : true;
+//   return value;
+// }
 
-console.log(largestSwap(14)); // false
+// console.log(largestSwap(14)); // false
+
+function Palindrome(str) {
+  let re = /[\W_]/g;
+  let lowRegStr = str.toLowerCase().replace(re, '');
+  let reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
+}
+console.log(Palindrome('eye'));
